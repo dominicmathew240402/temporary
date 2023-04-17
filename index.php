01c42c6df10f6198baa2f3758eb807d4e163c7e6
dominicmathew240402 <58551304+dominicmathew240402@users.noreply.github.com>
2023-04-17 08:39:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php include('contents/header.php'); ?>
+        
+        <section class="slider-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="bd-example">
+                            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner" role="listbox">
+                                    <div class="carousel-item active">
+                                        <img class="d-block img-fluid" src="upload/b4a.jpg" alt="First slide" />
+                                        <div class="carousel-caption d-none d-md-block">
+                                            <span class="cat-title">Lifestyle</span>
+                                            <h3>Dress for Those In-Between Days</h3>
+                                            <p> Aenean a tempus est. Praesent et dapibus turpis. Mauris vulputate diam lorems</p>
+                                            <div class="blog-bottom text-center">
+                                                <a href="single.php" class="btn btn-primary">Continue Reading</a>
+                                            </div><!-- end bottom -->
+                                        </div>
+                                    </div>
+
+                                    <div class="carousel-item">
+                                        <img class="d-block img-fluid" src="upload/109667.png" alt="Second slide" />
+                                        <div class="carousel-caption d-none d-md-block">
+                                            <span class="cat-title">Fashion</span>
+                                            <h3>Summer Lettering Workshops</h3>
+                                            <p> Aenean a tempus est. Praesent et dapibus turpis. Mauris vulputate diam lorems</p>
+                                            <div class="blog-bottom text-center">
+                                                <a href="single.php" class="btn btn-primary">Continue Reading</a>
+                                            </div><!-- end bottom -->
+                                        </div>
+                                    </div>
+
+                                    <div class="carousel-item">
+                                        <img class="d-block img-fluid" src="upload/124302.png" alt="Three slide" />
+                                        <div class="carousel-caption d-none d-md-block">
+                                            <span class="cat-title">Blogging</span>
+                                            <h3>01. Free Endless numbered day</h3>
+                                            <p> Aenean a tempus est. Praesent et dapibus turpis. Mauris vulputate diam lorems</p>
+                                            <div class="blog-bottom text-center">
+                                                <a href="single.php" class="btn btn-primary">Continue Reading</a>
+                                            </div><!-- end bottom -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section>
+
+      <?php include('contents/notifyl.php');?>
+
+     <?php include('contents/footer.php');?>
